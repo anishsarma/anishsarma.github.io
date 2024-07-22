3eaba4f6784bb7aaae13c0a207a615519d29768e
anishsarma <62362168+anishsarma@users.noreply.github.com>
2024-07-21 23:30:07 -0400

    Update script.js

--- senspec/script.js
+++ senspec/script.js
@@ -75,9 +75,12 @@ function normalDist(x, mean, sd) {
 
 // Function to calculate TPR and FPR
 function calculateRates(threshold, mean1, sd1, mean2, sd2) {
-    const tpr = .5 - math.erf((threshold - mean2) / (Math.sqrt(2) * sd2)) / 2;
-    const fpr = .5 - math.erf((threshold - mean1) / (Math.sqrt(2) * sd1)) / 2;
-    return {tpr, fpr};
+    // For TPR, we want P(X > threshold | X ~ N(mean2, sd2^2))
+    const tpr = 0.5 + math.erf((threshold - mean2) / (Math.sqrt(2) * sd2)) / 2;
+    
+    // For FPR, we want P(X > threshold | X ~ N(mean1, sd1^2))
+    const fpr = 0.5 + math.erf((threshold - mean1) / (Math.sqrt(2) * sd1)) / 2;
+        return {tpr, fpr};
 }
 
 // Function to find the threshold for a given point on the ROC curve
